2bac70d53f0214ce6537e6659a0fd20224642f8e
Archie To <toanhminh0412@gmail.com>
2024-04-09 21:51:27 -0700

    Publish contact and footer

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -48,21 +48,20 @@ export default function Contact() {
                         </div>
                         <h2 className="text-3xl font-bold tracking-tight text-gray-900">Get in touch</h2>
                         <p className="mt-6 text-lg leading-8 text-gray-600">
-                        Proin volutpat consequat porttitor cras nullam gravida at. Orci molestie a eu arcu. Sed ut tincidunt
-                        integer elementum id sem. Arcu sed malesuada et magna.
+                        Please fill in the form, let me know what your needs are. I&apos;m here to help!
                         </p>
                         <dl className="mt-10 space-y-4 text-base leading-7 text-gray-600">
-                        <div className="flex gap-x-4">
+                        {/* <div className="flex gap-x-4">
                             <dt className="flex-none">
                             <span className="sr-only">Address</span>
                             <BuildingOffice2Icon className="h-7 w-6 text-gray-400" aria-hidden="true" />
                             </dt>
                             <dd>
-                            545 Mavis Island
+                            909 Darwin Ave
                             <br />
-                            Chicago, IL 99191
+                            Victoria, BC
                             </dd>
-                        </div>
+                        </div> */}
                         <div className="flex gap-x-4">
                             <dt className="flex-none">
                             <span className="sr-only">Telephone</span>
@@ -70,7 +69,7 @@ export default function Contact() {
                             </dt>
                             <dd>
                             <a className="hover:text-gray-900" href="tel:+1 (555) 234-5678">
-                                +1 (555) 234-5678
+                                +1 (778) 7006939
                             </a>
                             </dd>
                         </div>
@@ -81,7 +80,7 @@ export default function Contact() {
                             </dt>
                             <dd>
                             <a className="hover:text-gray-900" href="mailto:hello@example.com">
-                                hello@example.com
+                                toanhminh0412@gmail.com
                             </a>
                             </dd>
                         </div>
