40ddfb117e3d4bd8835946cf09f385724e3e12b8
Archie To <toanhminh0412@gmail.com>
2024-04-13 15:36:21 -0700

    Add Google Analytics

--- portfolio/src/app/layout.js
+++ portfolio/src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 import Navbar from "@/components/Navbar";
 
@@ -16,6 +17,15 @@ export default function RootLayout({ children }) {
             <head>
                 {/* Font Awesome icon */}
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+                {/* <!-- Google tag (gtag.js) --> */}
+                <Script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}></Script>
+                <Script>
+                    {`window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');`}
+                </Script>
             </head>
             <body className={inter.className}>
                 <Navbar/>
