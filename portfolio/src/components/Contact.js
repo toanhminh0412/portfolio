89794a29a72204e7966386ae9e1e49937bffdc7a
Archie To <toanhminh0412@gmail.com>
2024-08-15 19:15:09 -0700

    Add a missing id

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -1,5 +1,5 @@
 export default function Contact() {
     return (
-        <iframe src="https://www.easierform.com/viewform/archieto" width="100%" height="1800px"></iframe>
+        <iframe id="contact" src="https://www.easierform.com/viewform/archieto" width="100%" height="1800px"></iframe>
     )
 }
